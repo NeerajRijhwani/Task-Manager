27b476361453e68a6dffecb761477f4d12666e88
Neeraj <neeraj.rijhwani9@gmail.com>
2025-07-13 13:02:15 +0530

    Initialize backend with Express, MongoDB connection, and basic middleware setup

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app=express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, // Adjust this to your frontend's URL
+    credentials: true, // Allow cookies to be sent with requests
+}));
+app.use(express.json({ limit:'16kb'}));
+app.use(express.urlencoded({ extended: true, limit:'16kb' }));
+app.use(express.static("public"))
+app.use(cookieParser())
+
+// //routes
+// import userRouter from "./routes/user.routes.js"
+
+// //routes declaration
+// app.use("/api/v1/users" , userRouter)
+// //http://localhost:3000/api/v1/users/register
+
+
+export {app}
---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+import {app} from "./app.js"
+import connectDB from "./db/index.js";
+
+connectDB()
+.then( ()=>{
+    app.listen(process.env.PORT || 5000,()=>{
+         console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.error("MongoDB connection failed ",err)
+    process.exit(1)
+})
