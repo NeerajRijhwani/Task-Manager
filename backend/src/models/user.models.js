6210825cd9374e4f312e125887f0a12b88515074
Neeraj <neeraj.rijhwani9@gmail.com>
2025-08-07 12:32:51 +0530

    Add password verification and token generation methods and use of jwt to user model

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -32,4 +32,29 @@ userSchema.pre("save",async function(next){
     this.password=await bcrypt.hash(this.password,10)
     next();
 })
+userSchema.methods.IsPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        username:this.username,
+        email:this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 export const User = mongoose.model("User",userSchema)
