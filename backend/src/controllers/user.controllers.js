340c037e7b407ea4f2fd3829decff1dd4c483568
Neeraj <neeraj.rijhwani9@gmail.com>
2025-08-07 13:38:22 +0530

    Implement user login functionality with access and refresh token generation

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -39,9 +39,65 @@ const RegisterUser= AsyncHandler(async (req,res)=>{
         new ApiResponse(200, createduser , "User registered successfully")
     )
 })
+const generateAccessandRefreshToken=async(userid)=>{
+try {
+    const user = await User.findById(userid)
+    const accessToken= user.generateAccessToken()
+    const refreshToken= user.generateRefreshToken()
+    user.refreshToken=refreshToken
+    await user.save({validateBeforeSave:false})
+    return {accessToken,refreshToken}
+} catch (error) {
+    throw new ApiError(500,"Something went wrong")
+}
+}
+const LoginUser = AsyncHandler(async (req,res)=>{
+    //take info from frontend
+    //validation-not empty
+    // check if username/email exists in db
+    // if yes check password is correct or not
+    //Access and refresh token and send it through cookie
+    // user successfully logged in
+
+    const {email,password}=req.body
+    if(!email){
+        throw new ApiError(400,"Email is required")
+    }
+
+    const user=await User.findOne({email})
+    if(!user){
+        throw new ApiError(400,"User does not exists")
+    }
+    const IsPasswordValid= await user.IsPasswordCorrect(password)
+    if(!IsPasswordValid){
+        throw new ApiError(400,"Invalid User Credentials")
+    }
+
+    const{accessToken,refreshToken}=await generateAccessandRefreshToken(user._id)
+
+    const loggedinUser= await User.findById(user._id).select("-password -refreshToken")
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user:loggedinUser,accessToken,refreshToken
+            },
+            "User logged in Succesfully"
+        )
+    )
+
+})
 
 
 
 
-
-export{RegisterUser}
+export{RegisterUser,LoginUser}
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import{
-    RegisterUser
+    RegisterUser,
+    LoginUser
 } from "../controllers/user.controllers.js"
+import { verifyJWT } from "../middlewares/Auth.middleware.js";
 const router=Router()
 
 
 
 router.route("/register").post(RegisterUser)
+router.route("/login").post(LoginUser)
 export default router;
