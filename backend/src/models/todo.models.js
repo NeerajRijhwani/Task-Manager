48f2712d9cbbb72d67a03dc9ee1ec6c4dfb6887a
Neeraj <neeraj.rijhwani9@gmail.com>
2025-08-27 20:55:26 +0530

    Add Todo model with schema definition for task management

---
+++ backend/src/models/todo.models.js
@@ -0,0 +1,35 @@
+import mongoose , {Schema} from "mongoose";
+
+const todoSchema= new Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    date:{
+        type:String,
+        required:true,
+    },
+    priority:{
+        type:String,
+        enum:["High","Moderate","Low"],
+        default:"High"
+    },
+    description:{
+        type:String,
+    },
+    taskimage:{
+        type:String,
+    },
+    status:{
+        type:String,
+        enum:["Not Started","Pending","Completed"],
+        default:"Not Started"
+    },
+    owner:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User"
+    }
+},{timestamps:true})
+
+export const Todo = mongoose.model("Todo",todoSchema)
