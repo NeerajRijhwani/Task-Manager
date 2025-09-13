7b2466366b560f5422f3c1c9664b8566cb0761ea
Neeraj <neeraj.rijhwani9@gmail.com>
2025-09-13 20:48:19 +0530

    add GetTasks functionality and updated routes

--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -1,34 +1,39 @@
 import { Router } from "express";
-import{
-    AddTask,
-    DeleteTask,
-    UpdateTaskDetails,
-    UpdateTaskImage
+import {
+  AddTask,
+  DeleteTask,
+  GetTasks,
+  UpdateTaskDetails,
+  UpdateTaskImage,
 } from "../controllers/todo.controllers.js";
 import { verifyJWT } from "../middlewares/Auth.middleware.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js";
 
-const router=Router()
+const router = Router();
 
-router.route("/addtask").post(verifyJWT,
-    upload.fields([
-        {
-            name: "taskimage",
-            maxCount:1
-        }
-    ])
-    ,AddTask)
+router.route("/addtask").post(
+  verifyJWT,
+  upload.fields([
+    {
+      name: "taskimage",
+      maxCount: 1,
+    },
+  ]),
+  AddTask
+);
 
-    router.route("/deleteTask/:_id").delete(verifyJWT,DeleteTask)
-    router.route("/updatetodo/:_id").patch(verifyJWT,UpdateTaskDetails)
-    router.route("/updatetodoimage/:_id").patch(
-        verifyJWT,
-        upload.fields([
-        {
-            name: "taskimage",
-            maxCount:1
-        }
-    ]),
-        UpdateTaskImage)
+router.route("/deleteTask/:_id").delete(verifyJWT, DeleteTask);
+router.route("/updatetodo/:_id").patch(verifyJWT, UpdateTaskDetails);
+router.route("/updatetodoimage/:_id").patch(
+  verifyJWT,
+  upload.fields([
+    {
+      name: "taskimage",
+      maxCount: 1,
+    },
+  ]),
+  UpdateTaskImage
+);
+router.route("/Tasks").get(verifyJWT, GetTasks);
 
 export default router;
