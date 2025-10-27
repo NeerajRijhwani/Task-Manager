b5e2d77520c66c5f7dfced1f56ca9062da47bbcd
Neeraj <neeraj.rijhwani9@gmail.com>
2025-10-27 13:30:46 +0530

    Authorization middleware for ensuring authorized admin commands

--- backend/src/middlewares/Auth.middleware.js
+++ backend/src/middlewares/Auth.middleware.js
@@ -1,42 +1,52 @@
-import {ApiError} from "../utils/ApiError.js";
-import {AsyncHandler} from "../utils/AsyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { AsyncHandler } from "../utils/AsyncHandler.js";
 import jwt from "jsonwebtoken";
-import {User} from "../models/user.models.js"
+import { User } from "../models/user.models.js";
 import { Organization } from "../models/organization.models.js";
 
-export const verifyJWT = AsyncHandler(async (req,res,next)=>{
-try {
-    const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-    
-    if(!token){
-        throw new ApiError(401,"Unauthorized Access")
-    }
-    const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
-    const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
+export const verifyJWT = AsyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
 
-    if(!user){
-        throw new ApiError(401,"Invalid Access Token")
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Access");
     }
-    req.user=user;
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+    req.user = user;
     next();
-} catch (error) {
-    throw new ApiError(401,error?.message|| "Invalid Access Token")
-}
-})
-export const verifyAuthorization=AsyncHandler(async(req,res,next)=>{
-    try {
-        const orgid=req.params._id;
-        const org= await Organization.findById(orgid);
-        if(!org){
-            throw new ApiError(400,"Organization not found");
-        }
-        const checkcall=org.members.includes([{user:req.user._id, role:"admin"}])
-        if(!checkcall){
-            throw new ApiError(401,"UnAuthorized Request Detected");
-        }
-        req.org=org;
-        next();
-    } catch (error) {
-         throw new ApiError(401,error?.message||"UnAuthorized Request Detected");
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
+export const verifyAuthorization = AsyncHandler(async (req, res, next) => {
+  try {
+    const orgid = req.params._id;
+    const org = await Organization.findById(orgid);
+    if (!org) {
+      throw new ApiError(400, "Organization not found");
     }
-})
+    const checkcall = org.members.some(x=> x.user.toString()=== req.user._id.toString() && x.role==="admin")
+    if (!checkcall) {
+        console.log({
+      user: req.user._id,
+      role: "admin",
+      _id:org._id
+    })
+    console.log(org.members)
+      throw new ApiError(401, "UnAuthorized Request Detected");
+    }
+    req.org = org;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "UnAuthorized Request Detected");
+  }
+});
--- backend/src/routes/organization.routes.js
+++ backend/src/routes/organization.routes.js
@@ -5,12 +5,28 @@ import {
   AddOrganizationMember,
   UpdateMemberRole,
   DeleteOrganization,
-} from "../controllers/organization.controllers";
-import { verifyJWT ,verifyAuthorization} from "../middlewares/Auth.middleware.js";
+} from "../controllers/organization.controllers.js";
+import {
+  verifyJWT,
+  verifyAuthorization,
+} from "../middlewares/Auth.middleware.js";
 const router = Router();
 //routes need to be tested
-router.route("/createorg").post(verifyJWT,CreateOrganization)
-router.route("/addorgmember/:_id").post(verifyJWT,verifyAuthorization,AddOrganizationMember)
-router.route("/deleteorgmember/:_id").delete(verifyJWT,verifyAuthorization,DeleteOrganizationMember)
-router.route("/updaterole/:_id").patch(verifyJWT,verifyAuthorization,UpdateMemberRole)
-router.route("/deleteorg/:_id").delete(verifyJWT,verifyAuthorization,DeleteOrganization)
+//create route is successfull
+router.route("/createorg").post(verifyJWT, CreateOrganization);
+  // add orgmember route is successfull
+router
+  .route("/addorgmember/:_id")
+  .post(verifyJWT, verifyAuthorization, AddOrganizationMember);
+router
+  .route("/deleteorgmember/:_id")
+  .delete(verifyJWT, verifyAuthorization, DeleteOrganizationMember);
+  //updaterole route is successful
+router
+  .route("/updaterole/:_id")
+  .patch(verifyJWT, verifyAuthorization, UpdateMemberRole);
+router
+  .route("/deleteorg/:_id")
+  .delete(verifyJWT, verifyAuthorization, DeleteOrganization);
+
+export default router;
